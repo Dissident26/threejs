6d77ef2002f8b2e6ab62a552724b327cefbd268a
Dissident26 <blackhate@bk.ru>
2024-07-01 21:20:01 +0300

    add movement animation

--- src/objects/characters/player.ts
+++ src/objects/characters/player.ts
@@ -4,9 +4,12 @@ import { TextureAnimator } from '../../texture-animator';
 import { playerSettings } from '../../settings';
 
 export const createPlayer = () => {
-  const texture = new TextureLoader().load('player/Idle.png');
+  const textureLoader = new TextureLoader();
+  const texture = textureLoader.load('player/Idle.png');
+  const texture2 = textureLoader.load('player/Run.png');
 
   const animator = new TextureAnimator(texture, 4, 3, 12, 128);
+  const animator2 = new TextureAnimator(texture2, 5, 4, 20, 20);
 
   const material = new MeshBasicMaterial({
     side: FrontSide,
@@ -26,7 +29,19 @@ export const createPlayer = () => {
     render(direction: Vector3) {
       const isMoving = direction.length();
 
-      animator.update(15);
+      if (isMoving) {
+        if (material.map?.id !== texture2.id) {
+          material.map = texture2;
+        }
+
+        animator2.update(15);
+      } else {
+        if (material.map?.id !== texture.id) {
+          material.map = texture;
+        }
+
+        animator.update(15);
+      }
 
       mesh.rotation.z = isMoving ? Math.atan2(direction.y, direction.x) + Math.PI / 2 : mesh.rotation.z;
       group.position.add(direction.multiplyScalar(playerSettings.speed));
