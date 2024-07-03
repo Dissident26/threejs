c0185d9bfcb18ba31cfb160773ca0250f5316dab
Dissident26 <blackhate@bk.ru>
2024-07-03 14:53:33 +0300

    refac texture loader

--- src/objects/characters/player.ts
+++ src/objects/characters/player.ts
@@ -1,22 +1,19 @@
-import { TextureLoader, MeshBasicMaterial, FrontSide, Group, PlaneGeometry, Mesh, Vector3, Quaternion } from 'three';
+import { MeshBasicMaterial, FrontSide, Group, PlaneGeometry, Mesh, Vector3, Quaternion } from 'three';
 
 import { TextureAnimator } from '../../texture-animator';
 import { playerSettings } from '../../settings';
+import { playerIdle, playerRun } from '../../textures';
 
 export const createPlayer = () => {
-  const textureLoader = new TextureLoader();
-  const texture = textureLoader.load('player/Idle.png');
-  const texture2 = textureLoader.load('player/Run.png');
-
-  const animator = new TextureAnimator(texture, 4, 3, 12, 128);
-  const animator2 = new TextureAnimator(texture2, 5, 4, 20, 20);
+  const animator = new TextureAnimator(playerIdle, 4, 3, 12, 128);
+  const animator2 = new TextureAnimator(playerRun, 5, 4, 20, 20);
 
   const quaternion = new Quaternion();
   const yAxis = new Vector3(0, -1, 0); //texture facing down
 
   const material = new MeshBasicMaterial({
     side: FrontSide,
-    map: texture,
+    map: playerIdle,
     transparent: true,
     color: playerSettings.color,
   });
@@ -33,15 +30,15 @@ export const createPlayer = () => {
       const isMoving = direction.length();
 
       if (isMoving) {
-        if (material.map?.id !== texture2.id) {
-          material.map = texture2;
+        if (material.map?.id !== playerRun.id) {
+          material.map = playerRun;
         }
 
         quaternion.setFromUnitVectors(yAxis, direction.clone().normalize());
         animator2.update(15);
       } else {
-        if (material.map?.id !== texture.id) {
-          material.map = texture;
+        if (material.map?.id !== playerIdle.id) {
+          material.map = playerIdle;
         }
 
         animator.update(15);
---
+++ src/textures/player.ts
@@ -0,0 +1,4 @@
+import { textureLoader } from './texture-loader';
+
+export const playerIdle = textureLoader.load('player/Idle.png');
+export const playerRun = textureLoader.load('player/Run.png');
