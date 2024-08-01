d84c6e8e7f8945c5b77e7e21211bc34d5d663119
Artem Zhukovsky <azhukovsky@enterrasolutions.com>
2024-08-01 13:56:07 +0200

    wrap async funcs in Promise.all

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,12 @@ const camera = createPerspectiveCamera();
 const playerAttack = createPlayerAttack();
 const crate = createCrate({ size: 0.5, position: new Vector3(0.5, 1) });
 const crate2 = createCrate({ size: 0.5, position: new Vector3(1, 0.5) });
-const tree = await createDeadTree({ size: 0.1, position: new Vector3(-1, -1) });
-const tree2 = await createDeadTree({ size: 0.1, position: new Vector3(1, -1) });
-const tree3 = await createDeadTree({ size: 0.1, position: new Vector3(-1, 1) });
+
+const trees = await Promise.all([
+  createDeadTree({ size: 0.1, position: new Vector3(-1, -1) }),
+  createDeadTree({ size: 0.1, position: new Vector3(1, -1) }),
+  createDeadTree({ size: 0.1, position: new Vector3(-1, 1) }),
+]);
 
 handleWindowResize(camera, renderer);
 
@@ -28,7 +31,7 @@ player.group.add(playerAttack.group);
 
 scene.add(camera);
 scene.add(player.group);
-scene.add(crate, crate2, tree, tree2, tree3);
+scene.add(crate, crate2, ...trees);
 
 const animate = () => {
   const direction = new Vector3(...keyboardController.direction);
